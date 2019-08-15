fa24f2b23852fae5338d39d595376df880198407
Alejandro Munoz <amunoz797@gmail.com>
2019-08-14 20:26:05 -0400

    Updated build files

--- solidity-ctf/index.js
+++ solidity-ctf/index.js
@@ -2,8 +2,17 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+
 let app = express();
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
